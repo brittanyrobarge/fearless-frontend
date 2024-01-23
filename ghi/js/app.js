5a73c21a9f67c8932fdf1f754f029d5157facfba
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-01-23 11:41:33 -0500

    update2

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,27 @@
+function createCard(title, description, pictureUrl, start, end, location ) {
+  return `
+    <div class="card mb-4 shadow">
+      <img src="${pictureUrl}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <h6 class="card-subtitle mb-2 text-secondary">${location}</h6>
+        <p class="card-text">${description}</p>
+      </div>
+      <div class="card-footer">
+      <small class="text-body-secondary">${start} - ${end}</small>
+    </div>
+    </div>
+  `;
+}
+
+function createAlert(alert) {
+  return `
+  <div class="alert alert-warning" role="alert">
+      ${alert}
+  </div>
+  `
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
 
   const url = 'http://localhost:8000/api/conferences/';
@@ -6,24 +30,41 @@ window.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(url);
 
     if (!response.ok) {
-      // Figure out what to do when the response is bad
+      console.log(response)
+      const column = document.querySelector('.col')
+      column.innerHTML += createAlert("Conferences not available")
     } else {
       const data = await response.json();
+      for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+            console.log(details)
 
-      const conference = data.conferences[0];
-      const nameTag = document.querySelector('.card-title');
-      nameTag.innerHTML = conference.name;
+            const title = details.conference.name;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+            const start = new Date(details.conference.starts).toLocaleDateString()
+            const end = new Date(details.conference.ends).toLocaleDateString()
+            const location = details.conference.location.name
+            const html = createCard(title, description, pictureUrl, start, end, location);
+            const column = document.querySelectorAll('.col');
+            if (column[0].querySelectorAll('.card').length === column[1].querySelectorAll('.card').length &&
+                  column[0].querySelectorAll('.card').length === column[2].querySelectorAll('.card').length) {
+                  // All columns have the same number of cards
+                  column[0].innerHTML += html;
+              } else if (column[0].querySelectorAll('.card').length > column[1].querySelectorAll('.card').length) {
+                  column[1].innerHTML += html;
+              } else if (column[1].querySelectorAll('.card').length > column[2].querySelectorAll('.card').length) {
+                  column[2].innerHTML += html;
+              }
+          }
+        }
 
-      const detailUrl = `http://localhost:8000${conference.href}`;
-      const detailResponse = await fetch(detailUrl);
-      if (detailResponse.ok) {
-        const details = await detailResponse.json();
-        console.log(details);
       }
-
-    }
   } catch (e) {
-    // Figure out what to do if an error is raised
+      console.error(e);
+    createAlert(e)
   }
-
 });
