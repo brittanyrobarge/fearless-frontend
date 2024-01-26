31bdc4dfe025651f2bf34f6038f96673644aca89
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-01-26 15:37:38 -0500

    html done

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,70 +1,66 @@
-function createCard(title, description, pictureUrl, start, end, location ) {
+function createCard(dates, title, description, pictureUrl, location) {
   return `
-    <div class="card mb-4 shadow">
-      <img src="${pictureUrl}" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <h6 class="card-subtitle mb-2 text-secondary">${location}</h6>
-        <p class="card-text">${description}</p>
-      </div>
-      <div class="card-footer">
-      <small class="text-body-secondary">${start} - ${end}</small>
-    </div>
-    </div>
+       <div class="card mb-4 shadow-lg">
+          <img src="${pictureUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
+            <p class="card-text">${description}</p>
+          </div>
+              <div class="card-footer">
+              <p>${dates.starts} - ${dates.ends}</p>
+              </div>
+          </div>
   `;
 }
 
-function createAlert(alert) {
-  return `
-  <div class="alert alert-warning" role="alert">
-      ${alert}
-  </div>
-  `
-}
 
 window.addEventListener('DOMContentLoaded', async () => {
-
-  const url = 'http://localhost:8000/api/conferences/';
+  const url ='http://localhost:8000/api/conferences/';
 
   try {
-    const response = await fetch(url);
+      const response = await fetch(url);
+      if (!response.ok) {
+          const rowTag = document.querySelector(".row");
+          const errorHtml = `<div class="alert alert-danger" role="alert">
+          Your URL is bad
+        </div>`
+          rowTag.innerHTML = errorHtml;
+      } else {
+          const data = await response.json();
 
-    if (!response.ok) {
-      console.log(response)
-      const column = document.querySelector('.col')
-      column.innerHTML += createAlert("Conferences not available")
-    } else {
-      const data = await response.json();
-      for (let conference of data.conferences) {
-          const detailUrl = `http://localhost:8000${conference.href}`;
-          const detailResponse = await fetch(detailUrl);
-          if (detailResponse.ok) {
-            const details = await detailResponse.json();
-            console.log(details)
+          for (let index=0; index< data.conferences.length; index++) {
+              const conference = data.conferences[index];
 
-            const title = details.conference.name;
-            const description = details.conference.description;
-            const pictureUrl = details.conference.location.picture_url;
-            const start = new Date(details.conference.starts).toLocaleDateString()
-            const end = new Date(details.conference.ends).toLocaleDateString()
-            const location = details.conference.location.name
-            const html = createCard(title, description, pictureUrl, start, end, location);
-            const column = document.querySelectorAll('.col');
-            if (column[0].querySelectorAll('.card').length === column[1].querySelectorAll('.card').length &&
-                  column[0].querySelectorAll('.card').length === column[2].querySelectorAll('.card').length) {
-                  // All columns have the same number of cards
-                  column[0].innerHTML += html;
-              } else if (column[0].querySelectorAll('.card').length > column[1].querySelectorAll('.card').length) {
-                  column[1].innerHTML += html;
-              } else if (column[1].querySelectorAll('.card').length > column[2].querySelectorAll('.card').length) {
-                  column[2].innerHTML += html;
+              const detailURL = `http://localhost:8000${conference.href}`;
+              const detailResponse = await fetch(detailURL);
+
+              if (detailResponse.ok) {
+                  const details = await detailResponse.json();
+                  const title = details.conference.name;
+                  const description = details.conference.description;
+                  const pictureUrl = details.conference.location.picture_url;
+                  const startDate = new Date(details.conference.starts).toLocaleDateString();
+                  const endDate = new Date(details.conference.ends).toLocaleDateString();
+                  const location = details.conference.location.name;
+                  const html = createCard({starts: startDate, ends: endDate}, title, description, pictureUrl, location);
+                  // const column = document.querySelector(".row.g-2");
+                  // column.innerHTML += html;
+
+                  const col_index = index % 3;
+                  const columns = document.querySelectorAll(".col");
+                  columns[col_index].innerHTML += html;
               }
-          }
-        }
 
+
+          }
       }
-  } catch (e) {
-      console.error(e);
-    createAlert(e)
+  } catch (error) {
+      const rowTag = document.querySelector(".row");
+          const errorHtml = `<div class="alert alert-danger" role="alert">
+          ${error}
+        </div>`
+      rowTag.innerHTML = errorHtml;
   }
+
 });
