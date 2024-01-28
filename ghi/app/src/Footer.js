b88d902098831ad256bd2249b09a479bc220a376
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-01-28 18:54:22 -0500

    app

---
+++ ghi/app/src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className=" container fixed-bottom mb-3 text-center">
+            @ 2024 Copyright:  <a className="text-reset fw-bold" href="http://gitlab.com/funksy" target="_blank">Gitlab.com/Funksy</a>
+        </footer>
+    );
+}
+
+export default Footer;
